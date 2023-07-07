4abc2035a2ac4df4009ef05dfba70521b0a70757
Avani Shah <127478301+avanishah111@users.noreply.github.com>
2023-07-07 23:56:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def main():
         """
        <style>
        .stApp {
-          background-color: lightblue;
+          background-color: ivory;
       }
       </style>
       """,
@@ -64,7 +64,7 @@ def main():
         success_message = f'Price of the car is {predictions[0]} INR'
 
         # Add color to the success message
-        colored_message = f'<span style="color: black;">{success_message}</span>'
+        colored_message = f'<span style="color: black;",font-size: 24px;">{success_message}</span>'
         st.markdown(colored_message, unsafe_allow_html=True)
 
 # Run the web app
